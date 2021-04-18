513b8b5baac0213d4d5c7e74ade98899402e6dcc
sydney <sydneychase5841@gmail.com>
2021-04-17 22:36:06 -0400

    created monstr div in js

--- js/index.js
+++ js/index.js
@@ -13,4 +13,25 @@ return fetch(url)
 }
 // const monsters = getMonsters();
 // console.log(monsters);
-getMonsters(3).then(data => console.log(data));
+getMonsters(1).then(data => console.log(data));
+
+/* <div>
+      <h2>Monster Name1</h2>
+      <h3>Monster Age1</h3>
+      <p>About the Monster2:</p>
+</div> */
+
+function createMonstersDiv(monsterObj) {
+    const div = document.createElement('div'),
+    h2 = document.createElement('h2'),
+    h3 = document.createElement('h3'),
+    p = document.createElement('p');
+
+    h2.textContent = monsterObj.name;
+    h3.textContent = monsterObj.age;
+    p.textContent = monsterObj.description;
+
+    div.appendChild(h2);
+    div.appendChild(h3);
+    div.appendChild(p);;
+}
