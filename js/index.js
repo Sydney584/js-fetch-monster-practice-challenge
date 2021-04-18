95a9e75d0af788c10b91cbb10c63deb61f99eef0
sydney <sydneychase5841@gmail.com>
2021-04-17 21:40:15 -0400

    created getMonsters func

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-const BASE_URL = "http://localhost:3000/"
+const BASE_URL = "http://localhost:3000/";
+
+// const url = `${BASE_URL}monsters/?_limit=50&_page=1`;
+// fetch(url)
+// .then(resp => resp.json())
+// .then(data => console.log(data));
+
+function getMonsters() {
+    const url = `${BASE_URL}monsters/?_limit=50&_page=1`;
+fetch(url)
+.then(resp => resp.json())
+.then(data => console.log(data));
+}
+getMonsters();
