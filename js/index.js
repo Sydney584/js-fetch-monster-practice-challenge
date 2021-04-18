0f5fa277e107c3a7777197d1e17fa1040e0a5cbf
sydney <sydneychase5841@gmail.com>
2021-04-17 22:02:50 -0400

    created getMonsters cb function

--- js/index.js
+++ js/index.js
@@ -5,10 +5,12 @@ const BASE_URL = "http://localhost:3000/";
 // .then(resp => resp.json())
 // .then(data => console.log(data));
 
-function getMonsters() {
-    const url = `${BASE_URL}monsters/?_limit=50&_page=1`;
-fetch(url)
+function getMonsters(page) {
+    const url = `${BASE_URL}monsters/?_limit=50&_page=${page}`;
+return fetch(url)
 .then(resp => resp.json())
 .then(data => console.log(data));
 }
-getMonsters();
+// const monsters = getMonsters();
+// console.log(monsters);
+getMonsters(3).then(data => console.log(data));
